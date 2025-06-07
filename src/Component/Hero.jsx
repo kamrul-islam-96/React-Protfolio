fea24c37eec64b247a538516cd669c681b07141e
kamrul-islam-96 <bullethasankamrul@gmail.com>
2025-06-07 22:11:43 +0600

    second commit

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -5,7 +5,32 @@ const Hero = () => {
     return (
         <div className='hero-section'>
             <div className="hero">
-                
+                <div className="left-side">
+                        <h1>
+                            Hi, my name is
+
+                            Alex Rahman  
+                            I build modern, scalable web applications.
+                        </h1>
+
+                        <p>
+                            I'm a passionate Full-Stack Web Developer with 3+ years of experience. I love creating intuitive UI, powerful backend systems, and solving real-world problems with code.
+
+                            Let's build something great together.
+                        </p>
+
+                        <div className="buttos">
+                            <button>View My Work</button>
+                            <button>Contact Me</button>
+                        </div>
+                </div>
+                <div className="right-side">
+                    <div className="image-div">
+                        <div className="img">
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
