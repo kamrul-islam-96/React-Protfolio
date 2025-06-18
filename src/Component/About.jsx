c94094dc1269180b196f843b9f0cc3d59794c022
kamrul-islam-96 <bullethasankamrul@gmail.com>
2025-06-18 14:26:06 +0600

    about and contact section

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -4,7 +4,12 @@ import '../Styles/index.css'
 const About = () => {
     return (
         <div className='about-section'>
-            
+            <div className="about-me">
+                <h1>About Me</h1>
+                <p>
+                    Hi, I'm Alex Rahman, a passionate Full Stack Web Developer with 4 years of experience in building dynamic, user-friendly web applications. I specialize in front-end technologies like HTML, CSS, JavaScript, and React, paired with back-end expertise in Node.js, Express, and MongoDB. My goal is to craft seamless digital experiences that solve real-world problems with clean, efficient code. I thrive on challenges, love learning new technologies, and take pride in delivering projects that exceed expectations. Whether it's designing responsive interfaces or building robust APIs, I'm here to bring ideas to life. Explore my projects to see my work, and feel free to reach out for collaboration!
+                </p>
+            </div>
         </div>
     );
 };
--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -4,7 +4,22 @@ import '../Styles/index.css'
 const Contact = () => {
     return (
         <div className='contact-section'>
-            
+            <div className="contact-form">
+                <div className="form-left">
+                    <h1>Contact</h1>
+                    <p>
+                        I would love to hear about your project and how I
+                        could help. Please fill in the form, and I'll get back
+                        to you as soon as possible.
+                    </p>
+                </div>
+                <form action="#">
+                    <input type="text" placeholder='Enter Your First Name' /> <br />
+                    <input type="text" placeholder='Enter Your Second Name' /> <br />
+                    <input type="email" placeholder='Enter Your Email' /> <br />
+                    <textarea name="message" placeholder="Your Message" rows="5" required></textarea>
+                </form>
+            </div>
         </div>
     );
 };
