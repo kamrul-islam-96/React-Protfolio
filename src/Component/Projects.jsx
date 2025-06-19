eda467254eb8c8fc7987d927af695d45f339e2a2
kamrul-islam-96 <bullethasankamrul@gmail.com>
2025-06-19 19:47:56 +0600

    Project section img adding

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -5,12 +5,24 @@ const Projects = () => {
     return (
         <div className='projects-section'>
             <div className="projects">
-                <div className="project-box"></div>
-                <div className="project-box"></div>
-                <div className="project-box"></div>
-                <div className="project-box"></div>
-                <div className="project-box"></div>
-                <div className="project-box"></div>
+                <div className="project-box">
+                    <img src="/public/images/Bitmap (1).jpg" alt="" />
+                </div>
+                <div className="project-box">
+                    <img src="/public/images/Bitmap (2).jpg" alt="" />
+                </div>
+                <div className="project-box">
+                    <img src="/public/images/Bitmap (3).jpg" alt="" />
+                </div>
+                <div className="project-box">
+                    <img src="/public/images/Bitmap (4).jpg" alt="" />
+                </div>
+                <div className="project-box">
+                    <img src="/public/images/Bitmap (5).jpg" alt="" />
+                </div>
+                <div className="project-box">
+                    <img src="/public/images/Bitmap (6).jpg" alt="" />
+                </div>
             </div>
         </div>
     );
