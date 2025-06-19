c9202e2b393ca6e8a8472563d87979a6244159bd
kamrul-islam-96 <bullethasankamrul@gmail.com>
2025-06-19 13:56:41 +0600

    Footer\

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -4,7 +4,11 @@ import '../Styles/index.css'
 const Footer = () => {
     return (
         <div className='footer'>
-            
+            <div className="footer-box">
+                <h1>
+                    @BulletHasan2025
+                </h1>
+            </div>
         </div>
     );
 };
